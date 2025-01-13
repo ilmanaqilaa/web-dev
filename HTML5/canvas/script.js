367b19c3087c5b3ce655c4e8f71bb51bbe673361
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2025-01-13 22:56:23 +0700

    UPDATE: Add rectangle, Arc, and Path shape for canvas implementation

--- HTML5/canvas/script.js
+++ HTML5/canvas/script.js
@@ -11,8 +11,28 @@ const c = mycanvas.getContext('2d')
 // Manipulasi Canvas
 
 
-// Buat rectangle 
-c.fillStyle = 'pink';
+// rectangle 
+c.fillStyle = 'orange';
 c.strokeStyle = '#999';
 c.lineWidth = '5';
 
+c.rect(50, 50, 300, 300);
+c.fill();
+c.stroke();
+
+// Circle/Arc
+c.fillStyle = 'blue';
+c.beginPath();
+c.arc(550, 200, 150, 0, 2 * Math.PI);
+c.fill();
+c.stroke();
+
+// Path/Triangle
+c.fillStyle = 'navy';
+c.beginPath();
+c.moveTo(900, 50);
+c.lineTo(1050, 350);
+c.lineTo(750, 350);
+c.closePath();
+c.stroke();
+c.fill();
