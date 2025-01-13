ece7eb435e373d04fca1ef31af29ae1c67079613
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2025-01-13 21:36:35 +0700

    UPDATE: Update js canvas

--- HTML5/canvas/script.js
+++ HTML5/canvas/script.js
@@ -5,4 +5,14 @@ const mycanvas = document.querySelector('#myCanvas');
 mycanvas.width = window.innerWidth;
 mycanvas.height = window.innerHeight;
 
-// 
+// Menentukan apakah canvas yang akan dibuat #D atau 2D
+const c = mycanvas.getContext('2d')
+
+// Manipulasi Canvas
+
+
+// Buat rectangle 
+c.fillStyle = 'pink';
+c.strokeStyle = '#999';
+c.lineWidth = '5';
+
