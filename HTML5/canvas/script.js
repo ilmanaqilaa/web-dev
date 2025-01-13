33d80e6243a6463b65de468254d737cdbff1742c
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2025-01-13 23:20:17 +0700

    UPDATE: Update for add animation to move shape.

--- HTML5/canvas/script.js
+++ HTML5/canvas/script.js
@@ -12,27 +12,57 @@ const c = mycanvas.getContext('2d')
 
 
 // rectangle 
-c.fillStyle = 'orange';
-c.strokeStyle = '#999';
-c.lineWidth = '5';
+// c.fillStyle = 'orange';
+// c.strokeStyle = '#999';
+// c.lineWidth = '5';
 
-c.rect(50, 50, 300, 300);
-c.fill();
-c.stroke();
+// c.rect(50, 50, 300, 300);
+// c.fill();
+// c.stroke();
+
+// // Circle/Arc
+// c.fillStyle = 'blue';
+// c.beginPath();
+// c.arc(550, 200, 150, 0, 2 * Math.PI);
+// c.fill();
+// c.stroke();
+
+// // Path/Triangle
+// c.fillStyle = 'navy';
+// c.beginPath();
+// c.moveTo(900, 50);
+// c.lineTo(1050, 350);
+// c.lineTo(750, 350);
+// c.closePath();
+// c.stroke();
+// c.fill();
 
 // Circle/Arc
+let x = 300;
+let y = 200;
+let speedX = 5;
+let speedY = 5;
+let radius = 150;
+
+function draw() {
+window.requestAnimationFrame(draw);
+c.clearRect(0, 0, innerWidth, innerHeight);
 c.fillStyle = 'blue';
 c.beginPath();
-c.arc(550, 200, 150, 0, 2 * Math.PI);
+c.arc(x, y, radius, 0, 2 * Math.PI);
 c.fill();
 c.stroke();
 
-// Path/Triangle
-c.fillStyle = 'navy';
-c.beginPath();
-c.moveTo(900, 50);
-c.lineTo(1050, 350);
-c.lineTo(750, 350);
-c.closePath();
-c.stroke();
-c.fill();
+if (x + radius > innerWidth || x - radius < 0) {
+    speedX = -speedX;
+}
+
+if (y + radius > innerHeight || y - radius < 0) {
+    speedY = -speedY;
+} 
+
+x += speedX;
+y += speedY;
+}
+
+draw();
